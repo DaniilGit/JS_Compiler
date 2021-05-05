09610c6c04c462520c313f74eeed09da6ac21de0
DaniilGit <daniil.work2019@gmail.com>
2021-05-05 13:59:44 +0700

    Add new examples

--- example/example2.js
+++ example/example2.js
@@ -3,5 +3,5 @@ let min = 0;
 
 for (let i = 0; i < array.length - 1; i += 1) {
   if (array[i] > array[i+1]) 
-    min = array[i+1];
+    min = i;
 }
---
+++ example/example8.js
@@ -0,0 +1,10 @@
+let b = 1;
+let c = 2;
+
+function f(b, c) {
+  let a = b + c;
+}
+
+f(b);
+
+g();
