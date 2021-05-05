b3c15423497045a596cae3e5d8078367451a9b1c
DaniilGit <daniil.work2019@gmail.com>
2021-05-05 14:01:58 +0700

    Change run files

--- run-test.sh
+++ run-test.sh
@@ -9,8 +9,12 @@ rm src/parser/test/parser-test/output.txt
 pytest -q src/parser/test/symtab-test/symtab_test.py
 rm src/parser/test/symtab-test/output.txt
 
+pytest -q src/parser/test/semantic-test/semantic_test.py
+rm src/parser/test/semantic-test/output.txt
+
 rm -R .pytest_cache
 rm -R src/parser/__pycache__
 rm -R src/parser/test/lexer-test/__pycache__
 rm -R src/parser/test/parser-test/__pycache__
 rm -R src/parser/test/symtab-test/__pycache__
+rm -R src/parser/test/semantic-test/__pycache__
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# python3 src/parser/js_lexer.py example/example1.js --out result.txt
-python3 src/parser/js_parser.py example/helloworld.js --out console
+# python3 src/parser/js_lexer.py example/example2.js --out result.txt
+python3 src/parser/js_parser.py example/example8.js --out console
 
 rm -R src/parser/__pycache__
+rm -R src/parser/symtab/__pycache__
